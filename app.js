ad65eb01415af1a2ed2e9beacafa0106f3a3d984
Jamief123 <45896675+Jamief123@users.noreply.github.com>
2019-08-28 04:57:54 +0100

    Added new route to show page. Not yet implemented     Removed unused packages improved styling of main page

--- app.js
+++ app.js
@@ -14,18 +14,39 @@ const API_KEY = process.env.TMDB_API_KEY;
 
 app.get("/", (req, res) => {
     var query = "language=en-US&page=1";
-    var url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=' + API_KEY + '&' + query;
-    request(url, (error, response, body) => {
-        if(!error && response.statusCode ==200){
+    var url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&${query}`;
+    request(url, (err, response, body) => {
+        if(!err && response.statusCode ==200){
             var data = JSON.parse(body);
             res.render('index', {data: data});
         }
     });
 });
 
-app.get("/movies/:search", (req, res) => {
-    var searchTerm = req.params.search;
-    res.render("results");
+app.get("/movies/:id", (req, res) => {
+    var id = req.params.id;
+    var url = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`;
+    request(url, (err, response, body) => {
+        if(!err && response.statusCode ==200){
+            var data = JSON.parse(body);
+            res.render('movies/show', {data: data});
+        }
+        else{
+            res.send("uh oh");
+        }
+    })
+});
+
+app.get("/search/:query", (req, res) => {
+    var query = req.params.query;
+    var url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}4&language=en-US&query=${query}&page=1&include_adult=false`;
+    request(url, (err, response, body) => {
+        if(!err && response.statusCode ==200){
+            var data = JSON.parse(body);
+            console.log(data);
+            res.render("results", {data: data});
+        }
+    })
 });
 
 app.listen(PORT, () => {
