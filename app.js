d72e8d84e915a311cbac474626c420c2df93d722
Jamief123 <45896675+Jamief123@users.noreply.github.com>
2019-08-28 16:37:47 +0100

    Updated show route to make it functional Improved styling to main page and show page Improved styling Improved styling. Will be improved later Improved styling of results page

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const dotenv = require("dotenv");
 const express = require("express");
+const bodyParser = require("body-parser");
 const request = require('request');
+
 var app = express();
 const PORT = 3000;
 
-app.set('view engine', 'ejs');
-
 dotenv.config();
+const APIKey = process.env.TMDB_API_KEY;
 
-const API_KEY = process.env.TMDB_API_KEY;
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
-//app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
+
 
 app.get("/", (req, res) => {
     var query = "language=en-US&page=1";
-    var url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&${query}`;
+    var url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${APIKey}&${query}`;
     request(url, (err, response, body) => {
         if(!err && response.statusCode ==200){
             var data = JSON.parse(body);
@@ -25,20 +28,21 @@ app.get("/", (req, res) => {
 
 app.get("/movies/:id", (req, res) => {
     var id = req.params.id;
-    var url = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`;
+    var url = `https://api.themoviedb.org/3/movie/${id}?api_key=${APIKey}&language=en-US`;
     request(url, (err, response, body) => {
         if(!err && response.statusCode ==200){
             var data = JSON.parse(body);
             res.render('movies/show', {data: data});
         }
         else{
-            res.send("uh oh");
+            console.log(err);
+            res.redirect("/");
         }
     })
 });
 
-app.get("/search/:query", (req, res) => {
-    var query = req.params.query;
+app.post("/search/", (req, res) => {
+    var query = req.body.Search;
     var url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}4&language=en-US&query=${query}&page=1&include_adult=false`;
     request(url, (err, response, body) => {
         if(!err && response.statusCode ==200){
