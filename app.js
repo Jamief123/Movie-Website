904eb6b55b0038ab33f5d8f9170c60ead8fb9ac9
Jamief123 <45896675+Jamief123@users.noreply.github.com>
2019-08-29 10:46:11 +0100

    Fixed bug that caused image carousel to disappear
    Add pagination to index page

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 
 
-app.get("/", (req, res) => {
-    var query = "language=en-US&page=1";
+app.get("/:page", (req, res) => {
+    var pageNum = req.params.page;
+    var query = `language=en-US&page=${pageNum}`;
     var url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${APIKey}&${query}`;
     request(url, (err, response, body) => {
         if(!err && response.statusCode ==200){
@@ -43,7 +44,7 @@ app.get("/movies/:id", (req, res) => {
 
 app.post("/search/", (req, res) => {
     var query = req.body.Search;
-    var url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}4&language=en-US&query=${query}&page=1&include_adult=false`;
+    var url = `https://api.themoviedb.org/3/search/movie?api_key=${APIKey}4&language=en-US&query=${query}&page=1&include_adult=false`;
     request(url, (err, response, body) => {
         if(!err && response.statusCode ==200){
             var data = JSON.parse(body);
@@ -51,6 +52,17 @@ app.post("/search/", (req, res) => {
             res.render("results", {data: data});
         }
     })
+});
+
+app.get("", (req, res) => {
+    var query = "language=en-US&page=1";
+    var url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${APIKey}&${query}`;
+    request(url, (err, response, body) => {
+        if(!err && response.statusCode ==200){
+            var data = JSON.parse(body);
+            res.render('index', {data: data});
+        }
+    });
 });
 
 app.listen(PORT, () => {
